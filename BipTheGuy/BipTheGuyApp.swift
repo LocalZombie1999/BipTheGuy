15d19d3ad3e611088fd2f06a98fde84b28cfb67c
LocalZombie1999 <r.annette.barrles08@gmail.com>
2026-04-08 14:47:47 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by BARRALES, ROXY on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
