4468b88ef9c75feda897251f4d9fdd4b503c5d79
LocalZombie1999 <r.annette.barrles08@gmail.com>
2026-04-08 15:05:45 -0700

    3.1 Video

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,19 +6,59 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var auidoPlayer: AVAudioPlayer!
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                }
+            Spacer()
+           
+            Button {
+                //TODO: Button action here :3
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+            .buttonStyle(.borderedProminent)
+
+
         }
         .padding()
     }
+    
+    func playSound(soundName: String){
+        
+        if auidoPlayer != nil && auidoPlayer.isPlaying{
+            auidoPlayer.stop()
+        }
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print (" ={ Could not read file \(soundName) ")
+            return
+        }
+                        
+        do{
+            auidoPlayer = try AVAudioPlayer(data: soundFile.data)
+            auidoPlayer.play()
+        }catch{
+            print (" ={ Error: \(error.localizedDescription) creating audioPlayer")                }
+    }
+    
 }
 
 #Preview {
     ContentView()
+}
+
+struct ContentView_Previews:PreviewProvider{
+    static var previews: some View {
+        ContentView()
+    }
 }
